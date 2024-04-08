86b2a6b0366071a4f7552ad8535b756c916e4902
MadLogarithm <1141519626@qq.com>
2024-04-08 16:04:28 +0800

    fix: patch from ycte

--- src/js/DictionaryInter.js
+++ src/js/DictionaryInter.js
@@ -10,6 +10,10 @@ function getJsDictionary() {
             en: 'Country-by-Country Laws',
             zh: '国别国法律',
         },
+        case: {
+            en: 'Country-by-Country Cases',
+            zh: '国别案例',
+        }
     }
     return dict
 }
@@ -18,6 +22,17 @@ function getJsLanguage() {
 }
 
 export const COUNTRY_BY_COUNTRY_LAWS = 10
+export const COUNTRY_BY_COUNTRY_CASES = 14
 export const reportTypeNameMap: { [key: number]: string } = {
-    [COUNTRY_BY_COUNTRY_LAWS]: getJsDictionary()['3'][getJsLanguage()]
+    [COUNTRY_BY_COUNTRY_LAWS]: getJsDictionary()['3'][getJsLanguage()],
+    [COUNTRY_BY_COUNTRY_CASES]: (() => {
+        console.log('>>>>>>>> reportTypeIdConst.js >>>>>>>>> getJsDictionary() >>>>>>>>>', getJsDictionary())
+        if (getJsDictionary().case
+            && typeof getJsDictionary().case === 'object'
+            && getJsDictionary().case[getJsLanguage()])
+            return getJsDictionary().case[getJsLanguage()]
+
+        else
+            return  '国别案例'
+    })(),
 }
